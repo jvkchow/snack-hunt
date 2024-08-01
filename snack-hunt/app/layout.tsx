8dff3891284b7229c64c301f525c6c72737783b4
jvkchow <jechow@ualberta.ca>
2024-07-31 21:12:51 -0600

    set up sign in and sign up buttons

--- snack-hunt/app/layout.tsx
+++ snack-hunt/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from './providers'
+import Provider from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- snack-hunt/app/page.tsx
+++ snack-hunt/app/page.tsx
@@ -6,21 +6,26 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
 
+        <div>
+          <Button className="bg-white text-black font-semibold text-lg">Sign In</Button>
+          <p className="py-4 m-0 max-w-[13ch] text-sm opacity-50">
+            Already have an account? Click here.
+          </p>
+        </div>
+
+        <div>
+          <Button className="bg-white text-black font-semibold text-lg">Sign Up</Button>
+          <p className="py-4 m-0 max-w-[13ch] text-sm opacity-50">
+            Don't have an account? Click here.
+          </p>
+        </div>
+
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Sign In{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Already have an account? Click here.
-          </p>
         </a>
 
         <a
@@ -29,15 +34,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Sign Up{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Don't have an account? Click here.
-          </p>
         </a>
       </div>
     </main>
